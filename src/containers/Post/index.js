f145c97d46d5e14eafdc7e636dbac460941cdcca
Loubna Kamal <loubnakamaly1998@gmail.com>
2023-08-19 17:13:38 +0300

    maybe last

--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './style.css';
 import Card from '../../components/UI/Card';
 import BlogPost from '../../components/BlogPost';
 import Sidebar from '../../components/SideBar';
