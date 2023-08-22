5d4bd141f2941a4fc5de8324010cd33e144d2aa5
Loubna Kamal <loubnakamaly1998@gmail.com>
2023-08-22 13:26:24 +0300

    final

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <Route path="/contact-us"  component={ContactUS}/>
         <Route path="/post/:slug" component={Post} />
         </Routes>
+        <p>tss</p>
 
         
       </div>
